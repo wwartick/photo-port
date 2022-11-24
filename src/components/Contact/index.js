b5e04d752af05c5fb6ca0f4d388ee05286f13fa2
wwartick <willwartick@gmail.com>
2022-11-23 21:22:15 -0800

    form onChange event Handlers

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState({name: '', email: '', message:''});
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
 
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id = "contact-form">
+            <form id = "contact-form" onSubmit={handleSubmit}>
 
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
 
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
 
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5"  />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
 
                 <button type="submit">Submit</button>
@@ -28,6 +35,5 @@ function ContactForm() {
         </section>
     );
 }
-
 
 export default ContactForm;
